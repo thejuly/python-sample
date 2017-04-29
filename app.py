68bdc35f48cfb874acac8172db12eb031182e960
thejuly <thejuly21574@gmail.com>
2017-04-30 06:23:45 +0700

    test

--- app.py
+++ app.py
@@ -1,23 +1,52 @@
-import os
-from flask import Flask
-
+# -*- coding: utf-8 -*-
+from flask import Flask, request
+import json
+import requests
+ 
 app = Flask(__name__)
-
+ 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return "Hello World!"
 
-@app.route('/tuna')
-def tuna():
-    return '<h2>Tuna is good</h2>'
-
-@app.route('/profile/<user>')
-def profile(user):
-    return '<h2>Tuna is good %s</h2>' % user
-
-@app.route('/post/<int:post_id>')
-def post(post_id):
-    return '<h2>Tuna is good %s</h2>' % post_id
+'''
+# ส่วน callback สำหรับ Webhook
+@app.route('/callback', methods=['POST'])
+def callback():
+    json_line = request.get_json()
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
+    user = decoded["events"][0]['replyToken']
+    #id=[d['replyToken'] for d in user][0]
+    #print(json_line)
+    print("ผู้ใช้：",user)
+    sendText(user,'งง') # ส่งข้อความ งง
+    return '',200
+ 
+def sendText(user, text):
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
+    Authorization = 'Bearer ENTER_ACCESS_TOKEN' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
+ 
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization':Authorization
+    }
+ 
+    data = json.dumps({
+        "replyToken":user,
+        "messages":[{
+            "type":"text",
+            "text":text
+        }]
+    })
+ 
+    #print("ข้อมูล：",data)
+    r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
+    #print(r.text)
+ 
+if __name__ == '__main__':
+     app.run(debug=True)
+'''
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
