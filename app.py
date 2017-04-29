5bfe16a33e058af0aa8c3882cf2da3e2c2874e49
thejuly <thejuly21574@gmail.com>
2017-04-30 05:35:07 +0700

    test

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello():
     return 'Hello World!'
 
-@app.route('/tuna')
-def tuna():
-    return '<h2>Tuna is good</h2>'
-
-@app.route('/profile/<user>')
-def profile(user):
-    return '<h2>Tuna is good %s</h2>' % user
-
-@app.route('/post/<int:post_id>')
-def post(post_id):
-    return '<h2>Tuna is good %s</h2>' % post_id
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
