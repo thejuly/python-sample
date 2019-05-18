2dfe116eacea6d6d0c0ed95e6f81b440dda9e621
thejuly <dcsbailey21574@gmail.com>
2019-05-19 00:37:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
+'''
 import os
 from flask import Flask
 
 app = Flask(__name__)
+'''
+
+import os
+#from flask import Flask
+
+
+
+from flask import Flask, request
+import json
+#import requests
+
+######################## Import ##############################
+
+
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
