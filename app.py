9ab267252a3979d36f89a60fc2fa252e845e254f
thejuly <dcsbailey21574@gmail.com>
2019-05-19 00:43:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,19 @@ app = Flask(__name__)
 def hello():
     return 'Hello World!'
 
+@app.route('/tuna')
+def tuna():
+    return '<h2>Tuna is good</h2>'
+
+@app.route('/profile/<user>')
+def profile(user):
+    return '<h2>Tuna is good %s</h2>' % user
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    return '<h2>Tuna is good %s</h2>' % post_id
+
+############### Basic & Test ################
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
