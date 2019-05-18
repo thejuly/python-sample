88e5c5fab8b3efc146506888f6cc0c59680ca776
thejuly <dcsbailey21574@gmail.com>
2019-05-19 01:03:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,20 +32,9 @@ def post(post_id):
 
 ###############################
 @app.route('/callback', methods=['POST'])
-def callback():
-    a = 123
-    
-    json_line = request.get_json()
-    json_line = json.dumps(json_line)
-    decoded = json.loads(json_line)
-    '''
-    user = decoded["events"][0]['replyToken']
-    #id=[d['replyToken'] for d in user][0]
-    #print(json_line)
-    print("ผู้ใช้：",user)
-    sendText(user,'งง') # ส่งข้อความ งง
-    return '',200
-    '''
+def aa():
+    a = 'b'
+
 ##############################
 
 
