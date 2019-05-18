25056e9b04ff1249426872d520047ec9b6b796a0
thejuly <dcsbailey21574@gmail.com>
2019-05-19 00:33:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,69 +1,11 @@
 import os
-#from flask import Flask
-
-
-
-from flask import Flask, request
-import json
-#import requests
-
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
-
-@app.route('/tuna')
-def tuna():
-    return '<h2>Tuna is good</h2>'
-
-@app.route('/profile/<user>')
-def profile(user):
-    return '<h2>Tuna is good %s</h2>' % user
-
-@app.route('/post/<int:post_id>')
-def post(post_id):
-    return '<h2>Tuna is good %s</h2>' % post_id
-
-###############################
-
-@app.route('/callback', methods=['POST'])
-def callback():
-    json_line = request.get_json()
-    json_line = json.dumps(json_line)
-    decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
-    #id=[d['replyToken'] for d in user][0]
-    #print(json_line)
-    print("ผู้ใช้：",user)
-    sendText(user,'งง') # ส่งข้อความ งง
-    return '',200
- 
-def sendText(user, text):
-    LINE_API = 'https://api.line.me/v2/bot/message/reply'
-    Authorization = 'Bearer ENTER_ACCESS_TOKEN' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
- 
-    headers = {
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization':Authorization
-    }
- 
-    data = json.dumps({
-        "replyToken":user,
-        "messages":[{
-            "type":"text",
-            "text":text
-        }]
-    })
- 
-    #print("ข้อมูล：",data)
-    r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
-    #print(r.text)
-
-##############################
-
-
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
