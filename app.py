0ea449bcdb08d68b22d9c519915b4919b4a3fc10
thejuly <dcsbailey21574@gmail.com>
2019-05-21 17:12:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,11 +29,313 @@ from linebot.exceptions import LineBotApiError
 
 
 
-app = Flask(__name__)
 
+
+
+
+
+
+app = Flask(__name__)
+#line_bot_api = LineBotApi('wbeBaLPb7xIuGymdaHU9yHy300QZ383XYgewhXLSoRe3TnlWo1xQuypNFpis1ExGrSTV1WpmtmQEiaR9tRPQHFUspwI9rVk2Ajfrg1WUwFpV9ewvq/vDx9LItfeNW+9y6Ih/OcwNpJPB/UfE9afIFwdB04t89/1O/w1cDnyilFU=') # Channel Access Token DCS-BOT
+#handler = WebhookHandler('49a9d31e3b8135ee7f85e6bc78848baa') # Channel Secret DCS-BOT
+
+line_bot_api = LineBotApi('CFaQZEnotjzhRjT4JMbcMyA0cksM4HE9Zu7HA8xQdWVbOMh6HLdeiCp2NKv6gmjNmJHs+NhWvnk8DYUZ3r8mCX6goDgYby/EV9s+/2mR9piHnPUVUGzWhRlmWw5K8RQG8iwgtSPnw89zEaUILnhtMwdB04t89/1O/w1cDnyilFU=') # Channel Access Token Report
+handler = WebhookHandler('f1e03e9bb185204d4494a1cce993970a') # Channel Secret Report
+#end Token
+
+# for test route
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Hello World! thongpoon Sarsaiy'
+
+@app.route('/tuna')
+def tuna():
+    return '<h2>Tuna is good</h2>'
+
+@app.route('/profile/<user>')
+def profile(user):
+    return '<h2>Tuna is good %s</h2>' % user
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    return '<h2>Tuna is good %s</h2>' % post_id
+
+@app.route('/temp')
+def temp():
+    userThongpoon = 'fdbc6747edd645dc9f77dca0eb120e94'
+    sendMessage = 'Temp report in period'
+    sendText(userThongpoon,sendMessage)
+    return '',200
+
+@app.route('/serv')
+def serv():
+    try:
+        cur.execute("""SELECT * from t_report""")
+    except:
+        print 'cannot select'
+
+    rows = cur.fetchall()
+    for row in rows:
+        testx = row[1]
+    
+    #return 'Hello World!'
+    return testx
+    #return '',200
+
+# end test route
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    # print("body:",body)   
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+
+
+
+
+
+
+@handler.default()
+def default(event):
+    print(event)
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    print("event.reply_token:", event.reply_token)
+    print("event.message.text:", event.message.text)
+
+    ########################################## Dcs report User ask ################################################
+    if event.message.text == "Serv":
+        msg = 'No Data'
+        try:
+            cur.execute("""SELECT * from t_report""")
+        except:
+            print 'cannot select'
+
+        rows = cur.fetchall()
+        for row in rows:
+            msg = row[1]
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+        return 0
+
+    if event.message.text == "Temp":
+        msg = 'No Data'
+        try:
+            cur.execute("""SELECT * from t_report""")
+        except:
+            print 'cannot select'
+
+        rows = cur.fetchall()
+        for row in rows:
+            msg = row[2]
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+        return 0
+
+    if event.message.text == "Rect":
+        msg = 'No Data'
+        try:
+            cur.execute("""SELECT * from t_report""")
+        except:
+            print 'cannot select'
+
+        rows = cur.fetchall()
+        for row in rows:
+            msg = row[3]
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+        return 0
+
+    if event.message.text == "Netw":
+        msg = 'No Data'
+        try:
+            cur.execute("""SELECT * from t_report""")
+        except:
+            print 'cannot select'
+
+        rows = cur.fetchall()
+        for row in rows:
+            msg = row[4]
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+        return 0
+
+    if event.message.text == "Cont":
+        msg = 'No Data'
+        try:
+            cur.execute("""SELECT * from t_report""")
+        except:
+            print 'cannot select'
+
+        rows = cur.fetchall()
+        for row in rows:
+            msg = row[5]
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+        return 0
+
+    if event.message.text == "Net":
+        msg = 'No Data'
+        try:
+            cur.execute("""SELECT * from t_report""")
+        except:
+            print 'cannot select'
+
+        rows = cur.fetchall()
+        for row in rows:
+            msg = row[6]
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+        return 0
+
+    if event.message.text == "Con":
+        msg = 'No Data'
+        try:
+            cur.execute("""SELECT * from t_report""")
+        except:
+            print 'cannot select'
+
+        rows = cur.fetchall()
+        for row in rows:
+            msg = row[7]
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+        return 0
+
+    if event.message.text == "Report":
+        msg = 'No Data'
+        try:
+            cur.execute("""SELECT * from t_report""")
+        except:
+            print 'cannot select'
+
+        rows = cur.fetchall()
+        for row in rows:
+            msg = row[8]
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+        return 0
+
+    if event.message.text == "Rpt":
+        msg = 'No Data'
+        try:
+            cur.execute("""SELECT * from t_report""")
+        except:
+            print 'cannot select'
+
+        rows = cur.fetchall()
+        for row in rows:
+            msg = row[9]
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+        return 0
+
+    if event.message.text == "Tmp":
+        msg = 'No Data'
+        try:
+            cur.execute("""SELECT * from t_report""")
+        except:
+            print 'cannot select'
+
+        rows = cur.fetchall()
+        for row in rows:
+            msg = row[10]
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+        return 0
+
+    if event.message.text == "Rct":
+        msg = 'No Data'
+        try:
+            cur.execute("""SELECT * from t_report""")
+        except:
+            print 'cannot select'
+
+        rows = cur.fetchall()
+        for row in rows:
+            msg = row[11]
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+        return 0
+    ########################################## Dcs report User ask ################################################
+    
+    
+    if event.message.text == "aa":
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='your text is aa'))
+        return 0
+
+    if event.message.text == "bb":
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+        return 0
+
+    if event.message.text == "cc":
+        #profile = line_bot_api.get_profile('C91f0e330efd3aebe03c34bfd2bd40cce')
+        #profile = line_bot_api.get_profile('U5e90b6b6d543d8d96be449d8fcd3ddbe')
+        #a = (profile.display_name)
+        #b = (profile.group_id) #for group_id
+        #c = (profile.picture_url)
+        #d = (profile.status_message)
+        line_bot_api.push_message('C22aaa682ea40676ee0f1d6fd120265b4', TextSendMessage(text='Hello MyJob'))
+        #line_bot_api.reply_message(event.reply_token, TextSendMessage(text=a))
+        return 0
+    
+    if event.message.text == "dd":
+        #profile = line_bot_api.get_profile('U5e90b6b6d543d8d96be449d8fcd3ddbe') #profile of sender => Thongpoon Auto 0684 (User)
+        #a = (profile.display_name)
+        #b = (profile.user_id) #for user_id
+        #c = (profile.picture_url)
+        #d = (profile.status_message)
+        #line_bot_api.push_message(b, TextSendMessage(text='Hello Thongpoon!'))
+        line_bot_api.push_message('Ub4c61cd90d77325b3cf97e4f9d881f26', TextSendMessage(text='Hello Thongpoon!'))
+        return 0
+
+
+    if event.message.text == "json":
+        
+        #Thongpoon
+        json_line = request.get_json()
+        json_line = json.dumps(json_line)
+        decoded = json.loads(json_line)
+
+        #user = user who send message to me
+        #messageGet = message that user send to me
+        #typeGet = type of data that user send to me
+        user = decoded["events"][0]['replyToken']
+        messageGet = decoded["events"][0]['message']['text']
+        typeGet = decoded["events"][0]['message']['type']
+        #Thongpoon
+    
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=json_line))
+        return 0
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
